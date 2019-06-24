0b75f970f602490e14ad4ba8b073abf9f24cfa1f
Your Name <you@example.com>
2019-06-24 16:19:50 -0300

    Padrão de criação de projeto

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,23 @@
 <template>
-  <div id="app">
+  <v-app id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>
     </div>
+    <div>
+      <v-btn color="success">Success</v-btn>
+      <v-btn color="error">Error</v-btn>
+      <v-btn color="warning">Warning</v-btn>
+      <v-btn color="info">Info</v-btn>
+    </div>
+
     <router-view/>
-  </div>
+  </v-app>
 </template>
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
